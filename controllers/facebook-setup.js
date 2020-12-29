afc55a9f51fa6c50498ea6dc26fa008ee9dce306
Pablo Melo <pablo@kona.tech>
2020-12-29 01:07:27 -0300

    adding interests and behaviors

--- controllers/facebook-setup.js
+++ controllers/facebook-setup.js
@@ -10,14 +10,64 @@ function getFacebookGeolocation(facebook_token,location_type,query) {
     request.get(options, (err, response, body) => {
       if (err || !response || response.statusCode !== 200) {
         console.log(err);
-         reject();
+         return reject();
       }
-      console.log(body);
-      resolve(body);
+      var result = JSON.parse(body);
+      if(result && result.data && location_type!='city')
+        result.data = result.data.sort((a, b) => (a.name > b.name) ? 1 : -1);
+      resolve(result);
+    });
+  });
+}
+
+function getFacebookBehaviors(facebook_token,query) { 
+
+  return new Promise((resolve, reject) => {
+    const options = {
+      url: `${global.conf.FACEBOOK_SEARCH_BASE_URL}/search?access_token=${facebook_token}&type=adTargetingCategory&class=behaviors&limit=1000&q=${query}`
+    };
+    
+    request.get(options, (err, response, body) => {
+      if (err || !response || response.statusCode !== 200) {
+        console.log(err);
+         return reject();
+      }
+      var result = JSON.parse(body);
+      result.data = result.data.sort((a, b) => (a.name > b.name) ? 1 : -1);
+      
+      resolve(result);
+    });
+  });
+}
+
+function getFacebookInterests(facebook_token,query) { 
+  var url = '';
+  if(query && query!='')
+    url = `${global.conf.FACEBOOK_SEARCH_BASE_URL}/search?access_token=${facebook_token}&type=adinterest&limit=1000&q=${query}`
+  else
+    url= `${global.conf.FACEBOOK_SEARCH_BASE_URL}/search?access_token=${facebook_token}&type=adTargetingCategory&class=interests&limit=1000`
+  
+  console.log('url',url);
+    return new Promise((resolve, reject) => {
+    const options = {
+      url 
+    };
+    
+    request.get(options, (err, response, body) => {
+      if (err || !response || response.statusCode !== 200) {
+        console.log(err);
+         return reject();
+      }
+      var result = JSON.parse(body);
+      result.data = result.data.sort((a, b) => (a.name > b.name) ? 1 : -1);
+      
+      resolve(result);
     });
   });
 }
 
 module.exports = {
-  getFacebookGeolocation
+  getFacebookGeolocation,
+  getFacebookBehaviors,
+  getFacebookInterests
 }
--- routes/facebook_setup.js
+++ routes/facebook_setup.js
@@ -15,7 +15,7 @@ router.get('/geolocation/', function(req, res, next) {
   var access_token = 'EAAB2i4QKEp0BACsMRCfxV2ul45X291SocBPOYaXGxeGaqsrWl4La2ByRPn1ZArr1ZCQZCnzwe9WMRqvrvuzEI9W7PIMwkP1h2khaT82XerAV0roOoqZAaqDDfcThFre7ZCTYYcLrZCeNK3SHcUFVxN7lX1WoqmsfZAkTn1zURkqZCwZDZD';
   facebookSetupController.getFacebookGeolocation(access_token,type,query)
   .then((result)=>{
-    res.send(JSON.parse(result));
+    res.send(result);
   })
   .catch((err) =>{
     console.log(err);
@@ -23,5 +23,36 @@ router.get('/geolocation/', function(req, res, next) {
   })
 });
 
+router.get('/behaviors/', function(req, res, next) {
+  
+  var access_token = 'EAAB2i4QKEp0BACsMRCfxV2ul45X291SocBPOYaXGxeGaqsrWl4La2ByRPn1ZArr1ZCQZCnzwe9WMRqvrvuzEI9W7PIMwkP1h2khaT82XerAV0roOoqZAaqDDfcThFre7ZCTYYcLrZCeNK3SHcUFVxN7lX1WoqmsfZAkTn1zURkqZCwZDZD';
+  
+  var query = req.query && req.query['q'] ? req.query['q']:'';
+  facebookSetupController.getFacebookBehaviors(access_token,query)
+  .then((result)=>{
+    res.send(result);
+  })
+  .catch((err) =>{
+    console.log(err);
+    next();
+  })
+});
+
+router.get('/interests/', function(req, res, next) {
+  
+  var access_token = 'EAAB2i4QKEp0BACsMRCfxV2ul45X291SocBPOYaXGxeGaqsrWl4La2ByRPn1ZArr1ZCQZCnzwe9WMRqvrvuzEI9W7PIMwkP1h2khaT82XerAV0roOoqZAaqDDfcThFre7ZCTYYcLrZCeNK3SHcUFVxN7lX1WoqmsfZAkTn1zURkqZCwZDZD';
+  
+  var query = req.query && req.query['q'] ? req.query['q']:'';
+
+  console.log('query',query);
+  facebookSetupController.getFacebookInterests(access_token,query)
+  .then((result)=>{
+    res.send(result);
+  })
+  .catch((err) =>{
+    console.log(err);
+    next();
+  })
+});
 
 module.exports = router;
