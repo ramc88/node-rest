1bcff3699ac1e88e6fe7bda0a8e7e714d60b46a1
Ramiro <rclavijo@paypal.com>
2021-05-22 22:51:59 -0300

    sending data by websocket from jobs to server

--- controllers/worker/jobs/fbApi.js
+++ controllers/worker/jobs/fbApi.js
@@ -1,23 +1,38 @@
 const axios = require('axios');
+const io = require("socket.io-client");
 
-const job = async (config, facebookSearchBase) => {
+const job = async (config, facebookSearchBase, socketConfig) => {
     // config must be stringified
-    const request = {
-        method: 'GET',
-        //url: `${global.config.FACEBOOK_SEARCH_BASE_URL}/act_${AD_ACCOUNT_ID}/delivery_estimate?access_token=${TOKEN}
-        //&include_headers=false&method=get&optimization_goal=REACH&pretty=0&suppress_http_code=1&targeting_spec=${config}`
-        url: `${facebookSearchBase}/act_43038309/delivery_estimate?access_token=EAAK9ynZCluTYBAKDxswwEp1ZBZBhag5TwU3NX3fufcSoIeopEUT3GJMFZAZBU5zJLWprqHSY2zNZByHBiizfTONydk6ZBaiHSjCs8iZBRN2OZBemjkg1if9O3yZCT7o7ZC9nosiDMez1OEYAirEOOKlSLXtU8eVuZC2TDQABMQayhzs6VoKItYBq0aEzisVrgv7oK7YZD
-        &include_headers=false&method=get&optimization_goal=REACH&pretty=0&suppress_http_code=1&targeting_spec=${config}`
+    console.log('--------------JOB-----------------')
+    if (!socketConfig || !socketConfig.server) {
+        throw new Error('Not socket config or server specified');
     };
-
-    try {
-        const response = await axios(request);
-        console.log('AXIOS RESPONSE:: ', response.status, response.data);
-        return response.data;
-    } catch (e) {
-        console.log('AXIOS ERROR:: ', e);
-        return e
+    if (!facebookSearchBase || typeof facebookSearchBase !== 'string') {
+        throw new Error('Invalid facebook search config');
     }
+
+    const socket = io(socketConfig.server);
+    socket.on("connect", async () => {
+        // either with send()
+        const request = {
+            method: 'GET',
+            //url: `${global.config.FACEBOOK_SEARCH_BASE_URL}/act_${AD_ACCOUNT_ID}/delivery_estimate?access_token=${TOKEN}
+            //&include_headers=false&method=get&optimization_goal=REACH&pretty=0&suppress_http_code=1&targeting_spec=${config}`
+            url: `${facebookSearchBase}/act_43038309/delivery_estimate?access_token=EAAK9ynZCluTYBAKDxswwEp1ZBZBhag5TwU3NX3fufcSoIeopEUT3GJMFZAZBU5zJLWprqHSY2zNZByHBiizfTONydk6ZBaiHSjCs8iZBRN2OZBemjkg1if9O3yZCT7o7ZC9nosiDMez1OEYAirEOOKlSLXtU8eVuZC2TDQABMQayhzs6VoKItYBq0aEzisVrgv7oK7YZD
+            &include_headers=false&method=get&optimization_goal=REACH&pretty=0&suppress_http_code=1&targeting_spec=${config}`
+        };
+        console.log('URL: ', request.url);
+        try {
+            const response = await axios(request);
+            console.log('AXIOS RESPONSE:: ', response.status, response.data);
+            socket.emit('insertDb', response.data)
+            return response.data;
+        } catch (e) {
+            console.log('AXIOS ERROR:: ', e);
+            return e
+        }
+    });
 };
 
-job(process.argv[2], process.argv[3]);
+console.log('ARGV:: ', process.argv);
+job(process.argv[2], process.argv[3], JSON.parse(process.argv[4]));
--- controllers/worker/worker.js
+++ controllers/worker/worker.js
@@ -1,8 +1,10 @@
 const bullInt = require('./bullIntegration');
 const Queue = require('bull');
-const fbApiJob = require('./jobs/fbApi');
+// const fbApiJob = require('./jobs/fbApi');
 const pm2Manager = require('./pm2Manager');
-
+const socketConfig = {
+  server: 'ws://localhost:4000/'
+}
 exports.start = async (app) => {
   try {
     console.log('starting worker', global.config.redis)
@@ -10,26 +12,26 @@ exports.start = async (app) => {
 
     jobs.process(global.config.batchSize, async (job) => {
       console.log('-----------PROCESSING JOB-----------', job.data);
-        if (job.attemptsMade === 2) {
-          job.data.log = job.failedReason;
-          job.data.processing_status = 2;
-          // console.log('JOB-->', job);
-          await bullInt.addToQueue('works', job.data, { priority: 2 });
-          await job.remove();
-        } else {
-          // eslint-disable-next-line no-undef
-          if (job.data.type === 'Facebook') {
-            const exeParams = {
-              file: `${__dirname}/jobs/fbApi.js`,
-              name: `Execution-${job.data._id}`,
-              args: [JSON.stringify(job.data.config)]
-            };
-            return await pm2Manager.createProcess(exeParams);
-          }
-        };
-        return {}
-      });
-    } catch (e) {
-      console.log(e)
-    }
+      if (job.attemptsMade === 2) {
+        job.data.log = job.failedReason;
+        job.data.processing_status = 2;
+        // console.log('JOB-->', job);
+        await bullInt.addToQueue('works', job.data, { priority: 2 });
+        await job.remove();
+      } else {
+        // eslint-disable-next-line no-undef
+        if (job.data.type === 'Facebook') {
+          const exeParams = {
+            file: `${__dirname}/jobs/fbApi.js`,
+            name: `Execution-${job.data._id}`,
+            args: [JSON.stringify(job.data.config), 'https://graph.facebook.com/v9.0', JSON.stringify(socketConfig)]
+          };
+          return await pm2Manager.createProcess(exeParams);
+        }
+      };
+      return {}
+    });
+  } catch (e) {
+    console.log(e)
+  }
 };
